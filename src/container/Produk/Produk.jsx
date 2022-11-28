68ad64b5975edcb4984b20beac9e7be06b7fcb91
kodingankeren <55055303+kodingankeren@users.noreply.github.com>
2022-11-29 06:22:22 +0700

    1 file modified

--- src/container/Produk/Produk.jsx
+++ src/container/Produk/Produk.jsx
@@ -1,13 +1,23 @@
 import React, { Component, Fragment } from 'react';
 import './Produk.css';
 class Produk extends Component {
+  state = {
+    qty: 6,
+  };
+  handleMinus = () => {
+    alert('minus');
+  };
+  handlePlus = () => {
+    alert('plus');
+  };
   render() {
     return (
       <Fragment>
         <h1>Produk</h1>
-        <button>-</button>
+        <div>Qty : {this.state.qty}</div>
+        <button onClick={this.handleMinus()}>-</button>
         <input type="text" />
-        <button>+</button>
+        <button onClick={this.handlePlus()}>+</button>
       </Fragment>
     );
   }
