d1f0307152c8f43ad22a22344a05d58f88c181e8
kodingankeren <55055303+kodingankeren@users.noreply.github.com>
2022-11-29 02:35:15 +0700

    Initial commit

---
+++ src/component/BlogList/BlogList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './BlogList.css';
+const BlogList = () => {
+  return (
+    <section>
+      <h1>Title</h1>
+      <p>Deskripsi</p>
+    </section>
+  );
+};
+
+export default BlogList;
