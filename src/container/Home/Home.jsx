6811860525c513afe03ea0e70cf5ade1ca32c586
kodingankeren <55055303+kodingankeren@users.noreply.github.com>
2022-11-29 09:04:45 +0700

    2 files modified

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,12 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './Home.css';
 import Produk from '../../container/Produk/Produk';
 class Home extends Component {
+  state = {
+    cart: 0,
+  };
+  addToCart = (qty, count) => {
+    let new_value = 0;
+    if (count == '+') {
+      new_value = this.state.cart + qty;
+    } else if (count == '-') {
+      new_value = this.state.cart - qty;
+    }
+    this.setState({
+      cart: new_value,
+    });
+  };
   render() {
     return (
-      <main>
-        <Produk judul="Si Bolang" deskripsi="Bocah Layang-layang" />
-      </main>
+      <Fragment>
+        <b>Cart : {this.state.cart}</b>
+        <hr />
+        <Produk onQtyChange={(value, count) => this.addToCart(value, count)} />
+        <Produk onQtyChange={(value, count) => this.addToCart(value, count)} />
+        <Produk onQtyChange={(value, count) => this.addToCart(value, count)} />
+      </Fragment>
     );
   }
 }
--- src/container/Produk/Produk.jsx
+++ src/container/Produk/Produk.jsx
@@ -1,28 +1,43 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import './Produk.css';
 class Produk extends Component {
   state = {
-    qty: 7,
+    qty: 0,
+  };
+  handleAddToCart = (value, counter) => {
+    this.props.onQtyChange(value, counter);
   };
   handleMinus = () => {
-    this.setState({
-      qty: this.state.qty - 1,
-    });
+    if (this.state.qty > 0) {
+      this.setState(
+        {
+          qty: this.state.qty - 1,
+        },
+        () => {
+          this.handleAddToCart(1, '-');
+        }
+      );
+    }
   };
   handlePlus = () => {
-    this.setState({
-      qty: this.state.qty + 1,
-    });
+    this.setState(
+      {
+        qty: this.state.qty + 1,
+      },
+      () => {
+        this.handleAddToCart(1, '+');
+      }
+    );
   };
   render() {
     return (
-      <Fragment>
+      <div>
         <h1>Produk</h1>
         <div>Qty : {this.state.qty}</div>
         <button onClick={this.handleMinus}>-</button>
-        <input type="text" />
+        {/* <input type="text" /> */}
         <button onClick={this.handlePlus}>+</button>
-      </Fragment>
+      </div>
     );
   }
 }
