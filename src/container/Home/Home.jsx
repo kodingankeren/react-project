e92230e10aeb3f67a13f92112653ffe76c93de48
kodingankeren <55055303+kodingankeren@users.noreply.github.com>
2022-12-01 19:52:51 +0700

    Route Berhasil

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,11 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-  Outlet,
-} from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import axios from 'axios';
 import './Home.css';
 import HalamanDepan from '../../pages/HalamanDepan/HalamanDepan';
@@ -13,22 +7,22 @@ import Kontak from '../../pages/Kontak/Kontak';
 import Profile from '../../pages/Profile/Profile';
 import Navbar from '../../container/Navbar/Navbar';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HalamanDepan />,
-  },
-  {
-    path: '/kontak',
-    element: <Kontak />,
-  },
-]);
 class Home extends Component {
   render() {
     return (
       <Fragment>
-        <Navbar />
-        <RouterProvider router={router} />
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/kontak">Kontak</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path="/" element={<HalamanDepan />} />
+          <Route path="/kontak" element={<Kontak />} />
+        </Routes>
       </Fragment>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { BrowserRouter } from 'react-router-dom';
 import Home from './container/Home/Home';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <div>
-    <Home />
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
   </div>
 );
